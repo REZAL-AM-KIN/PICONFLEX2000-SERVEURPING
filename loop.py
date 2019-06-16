95b161eec6afdfc7bd0fc4a0dcfdbbe439a1ae21
REZAL KIN <48453769+REZALKIN@users.noreply.github.com>
2019-06-16 23:09:48 +0200

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,6 +1,5 @@
 print("Demarrage de 'loop.py'")
 _BOXS=SQL_SELECT("SELECT IP from boxs;")
-print(_BOXS)
 for i in range(len(_BOXS)):
     threading.Thread(target=REZAL_pingAndSetState,args=[_BOXS[i][0]]).start()
-sleep(1)
+sleep(5)
