52778f2e1a0e1ce58c2e729f37afda381e018b2f
REZAL KIN <48453769+REZALKIN@users.noreply.github.com>
2019-06-16 22:58:27 +0200

    Update loop.py

--- loop.py
+++ loop.py
@@ -3,8 +3,5 @@ _BOXS=SQL_SELECT("SELECT IP from boxs;")
 print(_BOXS)
 for i in range(len(_BOXS)):
     _t2=time()
-    _IP=_BOXS[i][0]
-    SQL_EXECUTE(QUERRY_setOnline(_IP,int(REZAL_ping(_IP))))
-    if time()-_t2<1:
-        sleep(0.1)
+    SQL_EXECUTE(QUERRY_setOnline(_IP,int(REZAL_ping(_BOXS[i][0]))))
 sleep(1)
